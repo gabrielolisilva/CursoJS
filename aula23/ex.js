a81304bfd383bff1408135f1cb9bc97557457544
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-03 15:55:11 -0300

    Exercício Aula 23

---
+++ aula23/ex.js
@@ -0,0 +1,7 @@
+let varA = 'A'; // tenha valor de varB
+let varB = 'B'; // tenha valor de varC
+let varC = 'C'; // tenha valor de varA
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
