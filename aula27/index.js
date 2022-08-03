530e4638a6f77776142152a6c5f2fa7886b9eb61
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-03 17:34:42 -0300

    Exercício 27

---
+++ aula27/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.54312;
+//let num2 = Math.floor(num1) arredonda para baixo
+//let num2 = Math.ceil(num1) arredonda para cima
+//let num2 = Math.Round(num1) arredondar para o número mais próximo para cima ou para baixo
+
+
+console.log(Math.random()); //gerar números aleatórios
+
+console.log(Math.random() * (10 - 5) + 5) //gerar números aleatórios entre 10 e 5
