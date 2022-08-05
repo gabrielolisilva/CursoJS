f2528c9d5430b09aa9c4cdcbde758b84d60650ea
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-05 11:20:58 -0300

    Aula32

---
+++ aula32/main.js
@@ -0,0 +1,14 @@
+const nome1 = 'Luiz'
+const sobrenome1 = 'Miranda'
+const idade1 = 25
+
+//isso de cima é a mesma coisa que isso debaixo
+
+const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25
+}
+
+console.log(pessoa1)
+console.log(pessoa1.nome)
