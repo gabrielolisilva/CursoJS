508b0e8a5b2be300082049e07c1144701701c1f2
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-09 13:48:40 -0300

    Exercício 50

---
+++ aula50/main.js
@@ -0,0 +1,10 @@
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+const [um, dois, tres, , cinco, ...resto] = numeros;
+//console.log(um, dois, tres, cinco)
+//console.log(resto)
+
+
+const valores = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const [lista1, lista2, lista3] = valores;
+console.log(lista1)
+console.log(lista2[2])
