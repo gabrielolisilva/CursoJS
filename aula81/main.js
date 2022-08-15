4d1ae71dc34c572b940a5fbca62d3b74d8812252
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-15 14:04:40 -0300

    Aula 81 Revisão Arrays Básica

---
+++ aula81/main.js
@@ -0,0 +1,16 @@
+/*
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+const novos = [...nomes]; //fazendo uma cópia de nomes para novos
+
+novos.pop();
+console.log(nomes);
+console.log(novos);
+*/
+
+const nome = 'Gabriel Oliveira Silva'; 
+const nomeArray = nome.split(' '); //transformar string em array
+console.log(nomeArray)
+/**********************************/
+const lista = ['Gabriel', 'Oliveira', 'Silva']
+const nomeLista = lista.join(' ');
+console.log(nomeLista)
