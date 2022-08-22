ef5887182907469f3c4b5d33c812b5834eddd85e
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-22 14:40:37 -0300

    Formulário de Cadastro

--- aula107/main.js
+++ aula107/main.js
@@ -1,16 +1,106 @@
-const botaoEnviar = document.querySelector('.enviar')
-const campoNome = document.getElementById("nome")
-const campoSobrenome = document.getElementById("sobrenome")
-const campoCPF = document.getElementById("cpf")
-const campoUsuario = document.getElementById("usuario")
-const campoSenha = document.getElementById("senha")
-const campoRepetirSenha = document.getElementById("repetirsenha")
-
-
-botaoEnviar.addEventListener('click', function(){
-    if(campoNome.value.length === 0 || campoSobrenome.value.length === 0 || campoCPF.value.length === 0 || campoUsuario.value.length === 0 || campoUsuario.value.length === 0 || campoSenha.value.length === 0 ||campoRepetirSenha.value.length === 0){
-        alert('Campo Vazio')
-    } else {
-        alert('Campo Preenchido')
+class ValidaFormulario {
+    constructor(){
+        this.formulario = document.querySelector('.formulario');
+        this.eventos();
     }
-})
+
+    eventos(){
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const camposValidos = this.camposSaoValidos();
+        const senhasValidas = this.senhaSaoValidas();
+
+        if(camposValidos && senhasValidas){
+            alert('Formulário enviado')
+            this.formulario.submit();
+        }
+    }
+
+    senhaSaoValidas(){
+        let valid = true;
+
+        const senha = this.formulario.querySelector('.senha');
+        const repetirsenha = this.formulario.querySelector('.repetirsenha');
+
+        if(senha.value !== repetirsenha.value) {
+            valid = false;
+            this.criaErro(senha, 'Senhas precisam ser iguais');
+            this.criaErro(repetirsenha, 'Senhas precisam ser iguais');
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12){
+            valid = false;
+            this.criaErro(senha, 'Senha precisa ter entre 6 e 12 caractéres')
+        }
+
+        return valid;
+    }
+
+    camposSaoValidos(){
+        let valid = true;
+
+        for(let errorText of this.formulario.querySelectorAll('.error-text')){
+            errorText.remove();
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            const label = campo.previousElementSibling.innerText;
+
+            if(!campo.value) {
+                this.criaErro(campo, `Campo ${label} não pode estar em branco`);
+                valid = false;
+            }
+
+            if(campo.classList.contains('cpf')){
+                if(!this.validaCPF(campo)) valid = false;
+            }
+
+            if(campo.classList.contains('usuario')){
+                if(!this.validaUsuario(campo)) valid = false;
+            }
+
+        }
+
+        return valid;
+    }
+
+    validaUsuario(campo){
+        const usuario = campo.value;
+        let valid = true;
+        if(usuario.length < 3 || usuario.length > 12){
+            this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caractéres.')
+            valid = false;
+        }
+
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
+            this.criaErro(campo, 'Nome de Usuário precisa conter apenas letras e/ou números')
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    validaCPF(campo){
+        const cpf = new ValidaCPF(campo.value);
+
+        if(!cpf.valida()){
+            this.criaErro(campo, 'CPF Inválido')
+            return false;
+        }
+        return true;
+    }
+
+    criaErro(campo, msg){
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
+    }
+}
+
+const valida = new ValidaFormulario();
