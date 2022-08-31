34d757a61bdd2a58f17f5f114a0d20afaf22ab09
gabrielolisilva <109990761+gabrielolisilva@users.noreply.github.com>
2022-08-31 14:32:24 -0300

    a

--- node/aula136/src/controllers/contatoController.js
+++
@@ -1,3 +0,0 @@
-exports.paginaInicial = (req, res) => {
-    res.send(`Olá você está na página de contatos`)
-}
--- node/aula136/src/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-//PÁGINA PARA O QUE VAI APARECER NA TELA
-exports.paginaInicial = (req, res) => {
-    //renderizando o arquivo index.ejs dentro de views
-    res.render('index');
-}
-
-exports.trataPost = (req, res) => {
-    res.send(`Seu formulário foi enviado ${req.body.nome}`)
-}
