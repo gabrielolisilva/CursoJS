58c798d42abbef5fffa0bf9cb7ff601d93ea6280
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-09-21 15:17:54 -0300

    Layout updates

---
+++ react/react-base/src/pages/Login/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    background: green;
+
+    small{
+        font-size: 12pt;
+        margin-left: 15px;
+        color: #999;
+    }
+`;
+
+//styled.p define que ele é uma tag p lá no index
+export const Paragrafo = styled.p` 
+    font-size: 80px;
+`;
